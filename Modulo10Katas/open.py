c678da76c9accf403f8b01f04668eb09e6b620dc
LuSegac <sgarciad1700@alumno.ipn.mx>
2022-02-17 21:13:31 -0600

    add Kata 10

---
+++ Modulo10Katas/open.py
@@ -0,0 +1,7 @@
+try:
+    open("config.txt")
+except OSError as err:
+    if err.errno == 2:
+         print("Couldn't find the config.txt file!")
+    elif err.errno == 13:
+         print("Found config.txt but couldn't read it")
